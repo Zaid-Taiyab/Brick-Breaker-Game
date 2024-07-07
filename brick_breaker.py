2eb56b477e6e82a5b4d7230f26715303372056fa
Zaid Taiyab <ztaiyab@asu.edu>
2024-07-07 01:46:29 -0700

    Update brick_breaker.py

--- brick_breaker.py
+++ brick_breaker.py
@@ -1,1 +1,108 @@
+import pygame
+import sys
+import random
+pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brick Breaker")
+
+clock = pygame.time.Clock()
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([100, 10])
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = (SCREEN_WIDTH - self.rect.width) // 2
+        self.rect.y = SCREEN_HEIGHT - 50
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 6
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 6
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > SCREEN_WIDTH - self.rect.width:
+            self.rect.x = SCREEN_WIDTH - self.rect.width
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([10, 10])
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.x = SCREEN_WIDTH // 2
+        self.rect.y = SCREEN_HEIGHT // 2
+        self.velocity = [random.randint(4, 8), random.randint(-8, 8)]
+
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+        if self.rect.x <= 0 or self.rect.x >= SCREEN_WIDTH - self.rect.width:
+            self.velocity[0] = -self.velocity[0]
+        if self.rect.y <= 0:
+            self.velocity[1] = -self.velocity[1]
+        if self.rect.y >= SCREEN_HEIGHT:
+            self.velocity = [0, 0]
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self, color, x, y):
+        super().__init__()
+        self.image = pygame.Surface([60, 20])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+paddle = Paddle()
+
+ball = Ball()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(paddle)
+all_sprites.add(ball)
+
+bricks = pygame.sprite.Group()
+
+for i in range(7):
+    for j in range(6):
+        brick = Brick(BLUE, 60 * i + 100, 30 * j + 50)
+        all_sprites.add(brick)
+        bricks.add(brick)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    all_sprites.update()
+
+    if pygame.sprite.collide_rect(ball, paddle):
+        ball.velocity[1] = -ball.velocity[1]
+
+    brick_collision_list = pygame.sprite.spritecollide(ball, bricks, True)
+    for brick in brick_collision_list:
+        ball.velocity[1] = -ball.velocity[1]
+
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
